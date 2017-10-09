d7b067bd237d0dacd5d30639e2ad942451919701
Adam <bata2_16@uni.worc.ac.uk>
2017-10-09 13:47:23 +0100

    fixed me being an idiot

--- www/js/index.js
+++ www/js/index.js
@@ -7,9 +7,9 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
 function updateDisplay() {
-	document.getElementById("launched").innerHTML("Application launched: " + launched_count);
-	document.getElementById("resumed").innerHTML("Application paused: " + paused_count);
-	document.getElementById("paused").innerHTML("Application resumed: " + resumed_count);
+	document.getElementById("launched").innerHTML = "Application launched: " + launched_count;
+	document.getElementById("resumed").innerHTML = "Application paused: " + paused_count;
+	document.getElementById("paused").innerHTML = "Application resumed: " + resumed_count;
 }
 
 
