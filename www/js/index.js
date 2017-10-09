3269f4239e3bdae39d850dd787f04af52714f383
Adam <bata2_16@uni.worc.ac.uk>
2017-10-09 13:45:19 +0100

    Changed to remove jquery

--- www/js/index.js
+++ www/js/index.js
@@ -2,21 +2,23 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+// here is ready listener
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
 function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
+	document.getElementById("launched").innerHTML("Application launched: " + launched_count);
+	document.getElementById("resumed").innerHTML("Application paused: " + paused_count);
+	document.getElementById("paused").innerHTML("Application resumed: " + resumed_count);
 }
 
 
 // device APIs are available
 //
     function onDeviceReady() {
-	
+	// here is resume listener
 	document.addEventListener("resume", onResume, false);
+        // here is pause listener
 	document.addEventListener("pause", onPause, false);
 	
 	launched_count++;
